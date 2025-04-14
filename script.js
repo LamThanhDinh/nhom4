8ac982c257defe41e6a3c211419a83a241c46b26
Khoa <phandangkhoawork@gmail.com>
2025-04-14 13:57:19 +0700

    Add feature_scripts

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('actionButton').addEventListener('click', function() {
+        alert("1. Lâm Thanh Đỉnh\n2. Nguyễn Nhật Hóa\n3. Phan Đăng Khoa");
+    });
+ <button id="actionButton">
+        <img src="D:\110122227\img\spike.png" alt="My selfie" style="width: 30px; height: 30px;">
+        Press me
+    </button>
